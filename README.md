b99e0685cc484ba04bd3c9af991376fc1a070e47
Rajesh M R <rajesh.mr82@gmail.com>
2022-12-04 11:25:47 +0530

    AOC 2022 - Day 4

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
-![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -83,16 +83,16 @@ Both parts: 4.5668ms
 
 ```
 Day 03
-Time part 1: 8.51ms
-Time part 2: 3.499ms
-Both parts: 12.0092ms
+Time part 1: 2.743ms
+Time part 2: 1.041ms
+Both parts: 3.7835ms
 ```
 
 ```
 Day 04
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.748ms
+Time part 2: 5.077ms
+Both parts: 5.8249ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 6/50
-Total time: 17.407ms
+Total stars: 8/50
+Total time: 15.006ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day04/index.ts
@@ -0,0 +1,70 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput.split("\n");
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  const contained = input.filter((pair) => {
+    const [elf1S1, elf1S2, elf2S1, elf2S2] = pair
+      .match(/(\d+)-(\d+),(\d+)-(\d+)/)
+      ?.slice(1, 5)
+      .map(Number);
+    return (
+      (elf1S1 <= elf2S1 && elf1S2 >= elf2S2) ||
+      (elf2S1 <= elf1S1 && elf2S2 >= elf1S2)
+    );
+  });
+
+  return contained.length;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  const overlap = input.filter((pair) => {
+    const [elf1S1, elf1S2, elf2S1, elf2S2] = pair
+      .match(/(\d+)-(\d+),(\d+)-(\d+)/)
+      ?.slice(1, 5)
+      .map(Number);
+    return elf1S1 <= elf2S2 && elf2S1 <= elf1S2;
+  });
+
+  return overlap.length;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8        
+        `,
+        expected: 2,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8           `,
+        expected: 4,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
