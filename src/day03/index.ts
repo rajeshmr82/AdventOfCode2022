7afa4c7a53e29ee6a5eff5709c2252943a402729
Rajesh M R <rajesh.mr82@gmail.com>
2022-12-05 10:22:35 +0530

    AOC 2022 - Day 3 - Refactor

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -5,29 +5,22 @@ const parseInput = (rawInput: string) => rawInput.split("\n");
 const commonItem = (sacks: string[]) => {
   const items: string[][] = sacks.map((sack) => [...new Set([...sack])]);
 
-  return items
-    .shift()
-    .filter((v) => {
-      return items.every((a) => {
-        return a.indexOf(v) !== -1;
-      });
-    })
-    .map((badge) => priority.indexOf(badge) + 1);
+  const [first, ...rest] = items;
+
+  return first
+    .filter((v) => rest.every((a) => a.indexOf(v) !== -1))
+    .map((badge) => priority.indexOf(badge));
 };
 
-const priority = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const priority = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
   return input
-    .map((items) => {
-      const [compartment1, compartment2] = items.match(
-        new RegExp(".{1," + items.length / 2 + "}", "g"),
-      );
-
-      return commonItem([compartment1, compartment2]);
-    })
+    .map(([compartment1, compartment2]) =>
+      commonItem([compartment1, compartment2]),
+    )
     .flat()
     .reduce((acc, curr) => acc + curr, 0);
 };
@@ -35,7 +28,7 @@ const part1 = (rawInput: string) => {
 const elvesToGroups = (sacks: string[], groupSize: number) => {
   const groups = [];
   while (sacks.length > 0) {
-    const chunk = sacks.splice(0, groupSize);
+    const [chunk, ...rest] = sacks.splice(0, groupSize);
     groups.push(chunk);
   }
   return groups;
